a113badcf4a138e617c3ca89c8f01f8b0e0c0f11
RojoF <rojof83@gmail.com>
2020-02-05 12:16:16 +0100

    Importación del proyecto Mi-Turno al repositorio GitHub

---
+++ app/src/main/java/com/example/mycolas/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mycolas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFab (View v) {
+        Intent intent = new Intent(this, InsertActivity.class);
+        startActivity(intent);
+    }
+}
